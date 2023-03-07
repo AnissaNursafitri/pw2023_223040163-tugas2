5ed57df619808ed27ec83a76ea8d574bda424cae
AnissaNursafitri <anissans185@gmail.com>
2023-03-07 22:34:08 +0700

    tugaskuliah

--- tugas/tugas3/3a.php
+++ tugas/tugas3/3a.php
@@ -1,12 +1,15 @@
 <?php 
-echo "<h4> Menghitung Luas Lingkaran</h4>"
+echo "<h4> Menghitung Luas Lingkaran</h4>";
 function hitungLuasLingkaran($r){
-
+    echo "jari jari = $r cm <br>";
+    echo "luas lingkaran = " . (3.14 * $r * $r) . " cm²";
 }
-echo "<h4>Menghitung Keliling Lingkaran</h4>"
-function hitungKelilingLingkaran($r){
-    
+hitungLuasLingkaran(10);
+echo "<h4> Menghitung Keliling Lingkaran</h4>";
+function hitungKelilingLingkaran($h){
+    echo "jari jari = $h cm <br>";
+    echo "keliling lingkaran = " . (3.14 * ($h+$h)) . " cm²";
 }
-
+hitungKelilingLingkaran (20)
 
 ?>
